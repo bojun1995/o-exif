6eab27502edc832fcde10ed6d11de7ad91f7dd6a
bojun1995 <bojun1995@foxmail.com>
2022-11-28 23:08:27 +0800

    [init]init file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 7777,
   },
-  plugins: [vue()]
+  plugins: [react()]
 })
