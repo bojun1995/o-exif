c659ba06da87ba3fc9095a7c3c6e3f8155f17f43
bojun1995 <bojun1995@foxmail.com>
2022-12-02 23:57:15 +0800

    [init]init naive-ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>App</div>
+  <div>
+    <div style="height: 50px; width: 50px">
+      <n-icon size="40" color="#0E7A0D">
+        <game-controller />
+      </n-icon>
+    </div>
+    <n-button type="success">
+      Success
+    </n-button>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { GameController } from '@vicons/ionicons5'
+import { NButton, NIcon } from 'naive-ui'
+
 </script>
 
 <style lang="scss" rel="stylesheet/scss" type="text/scss" scoped></style>
